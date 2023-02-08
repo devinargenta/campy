dc2a20e4899f46d13280f822cc08805d12b94656
Devin Argenta <devin@devinargenta.com>
2023-02-08 13:53:11 -0500

    Initial Commit

---
+++ camprey/campreyApp.swift
@@ -0,0 +1,17 @@
+//
+//  campreyApp.swift
+//  camprey
+//
+//  Created by Devin Argenta on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct campreyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
